2b9aeda05bf2684e59f609f2c4a033106ded1d1c
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2026-01-16 23:50:10 +0200

    Update Environment

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  baseUrl: "markethubdata-production.up.railway.app/products",
-  UserUrl: "markethubdata-production.up.railway.app/users",
-  cartUrl: 'markethubdata-production.up.railway.app/carts',
-  orderUrl: 'markethubdata-production.up.railway.app/orders',
-  contactUrl: 'markethubdata-production.up.railway.app/contacts'
+  baseUrl: "https://markethubdata-production.up.railway.app/products",
+  UserUrl: "https://markethubdata-production.up.railway.app/users",
+  cartUrl: 'https://markethubdata-production.up.railway.app/carts',
+  orderUrl: 'https://markethubdata-production.up.railway.app/orders',
+  contactUrl: 'https://markethubdata-production.up.railway.app/contacts'
 };
