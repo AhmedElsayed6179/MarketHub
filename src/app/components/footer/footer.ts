367a302727966f83d695861f618a1f5fcd5d4d51
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2026-03-23 03:51:18 +0200

    Update Footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { UserAuth } from '../../service/user-auth';
 import { CommonModule } from '@angular/common';
@@ -9,13 +9,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {
+export class Footer implements OnInit {
   today = new Date();
   isLoggedIn: boolean = false
+  showDownloadLink = false;
   constructor(private _UserAuth: UserAuth) {
     this._UserAuth.isLoggedIn$.subscribe(status => {
       this.isLoggedIn = status
     })
+  }
+
+  ngOnInit(): void {
+    const ua = navigator.userAgent || navigator.vendor || '';
+
+    const isApk =
+      /wv/i.test(ua) ||
+      /Version\/[\d.]+.*Chrome/i.test(ua) ||
+      /Median/i.test(ua) ||
+      (window as any).cordova !== undefined ||
+      (window as any).Capacitor !== undefined;
+
+    const isMobile = /Android|iPhone|iPad|iPod|IEMobile|BlackBerry|Opera Mini/i.test(ua);
+
+    this.showDownloadLink = isMobile && !isApk;
   }
 
   Logout() {
