f85220ae8e67cf14eb9fc2721b27b85f9e4ad7d0
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2026-03-23 03:39:53 +0200

    Update Terms

--- src/app/components/terms/terms.ts
+++ src/app/components/terms/terms.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-terms',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './terms.html',
   styleUrl: './terms.css',
 })
